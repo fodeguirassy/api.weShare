6a26419c009a6926dc09b2055f29e80800552c69
School <fodeguipro@gmail.com>
2017-10-05 14:25:15 +0200

    add error handler

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-const cors = require('cors')
+const cors = require('cors');
 
 module.exports = (app) => {
-	app.use(cors());
+	app.use(cors())
 	app.use('/users', require ('./users')(app));
 	app.use('/auth', require ('./auth')(app));
-	app.use('/contents', require('./contents')(app));
+	app.use('/contents', require('./contents')(app))
+	app.use((err, req, res, next) => {
+		console.error(err);
+		next()
+	});
 }
