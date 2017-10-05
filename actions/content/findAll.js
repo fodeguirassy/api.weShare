a226f242cc8f2adf6145a3dadc94bbb1a2a81418
School <fodeguipro@gmail.com>
2017-10-05 16:46:43 +0200

    include specialty in find content by name

--- actions/content/findAll.js
+++ actions/content/findAll.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
   const Content = app.models.Content
 
   return function(req, res, next) {
-    console.log('I am in normal findAll');
     Content.findAll({include : [{model : Specialty}]}).then((contents) => {
       if(contents){return res.send(contents)}
       return res.status(404).send('No content found')
--- middlewares/parsers/contents.js
+++ middlewares/parsers/contents.js
@@ -1,10 +1,10 @@
 module.exports = (app) => {
     const Content = app.models.Content;
-
+    const Specialty = app.models.Specialty;
     return(req,res,next) => {
-
         Content.findAll({
-          where : { title : { $like: '%'+req.params.title+'%' } }
+          where : { title : { $like: '%'+req.params.title+'%' } },
+          include : [{model : Specialty}]
         }).then((content) => {
           if(!content){ return res.status(404).send('No content found')}
           req.content = content
