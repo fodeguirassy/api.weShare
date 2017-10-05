3937904d6bf764a8a6c0bccb2cbf047c137430ef
School <fodeguipro@gmail.com>
2017-10-05 14:11:28 +0200

    add env var to set dialect sequelize

--- databases/mysql.js
+++ databases/mysql.js
@@ -1,9 +1,11 @@
 const Sequelize = require ('sequelize')
 
+const dialect = process.ENV.DB_DIALECT || 'mysql';
+
 module.exports = (app, resolve, reject) => {
-	
+
 	app.sequelize =  new Sequelize(app.settings.mysql.database,app.settings.mysql.user,app.settings.mysql.password,
-		{dialect: 'mysql'});
+		{dialect: dialect});
 	app.sequelize.authenticate()
 		.then(() => {
 			resolve();
