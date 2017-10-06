242fefddbe9d0126a579b1f872611339b04d0083
School <fodeguipro@gmail.com>
2017-10-06 09:43:05 +0200

    create content with a file

--- actions/medias/create.js
+++ actions/medias/create.js
@@ -4,9 +4,8 @@ module.exports = (app) => {
   return function create(req, res, next) {
       let media = {
           file: app.settings.url + app.settings.uploadsDir + req.savedfilename,
-          filename: req.file.filename,
+          filename: req.body.file.filename,
       };
-
       Media.build(media).save().then((media) => {
           if (!media) return res.status(500).send("Error saving file");
           res.send(media);
